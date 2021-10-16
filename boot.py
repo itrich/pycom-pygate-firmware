a5d64210a74671cd16d61b4841e209877930a82a
Eduard Itrich <eduard@itrich.net>
2021-10-16 20:12:06 +0200

    Add support for Pybytes

--- boot.py
+++ boot.py
@@ -4,6 +4,7 @@ import pycom
 
 # We want to save a little bit power
 # Disable Heartbeat LED
-pycom.heartbeat_on_boot(False)
+pycom.heartbeat_on_boot(True)
+pycom.pybytes_on_boot(True)
 # Proceed with main.py
 machine.main('main.py')
