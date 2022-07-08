397a2cc28b76ce1d37920af1dd4801a9dd1d70a0
Eduard Itrich <eduard@itrich.net>
2022-07-08 09:40:47 +0200

    Help with some debug messages and led flashes

--- boot.py
+++ boot.py
@@ -4,16 +4,10 @@ import pycom
 from _pybytes import Pybytes
 from _pybytes_config import PybytesConfig
 
-# We want to save a little bit power
-# Disable Heartbeat LED
-pycom.heartbeat_on_boot(False)
 # Enable Pybytes
 pybytes_conf = PybytesConfig().read_config()
 pybytes = Pybytes(pybytes_conf)
 pybytes.start()
-
-# disable debug messages
-machine.pygate_debug_level(1)
 
 # Proceed with main.py
 machine.main('main.py')
