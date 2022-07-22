d28927e0c6766f4d6c52b2d0c1c48590b92cf3b7
Eduard Itrich <eduard@itrich.net>
2022-07-22 07:57:38 +0200

    Hard-code GW EUI into config.json, don't use wifi at all

--- boot.py
+++ boot.py
@@ -1,5 +1,16 @@
 # boot.py -- run on boot-up
 import machine
+from network import ETH
+
+# Connect via ethernet
+eth = ETH()
+eth.init(hostname="itrich-pygate-001")
+eth.ifconfig(config="dhcp")
+
+while not eth.isconnected():
+    print('.', end='')
+    time.sleep(1)
+print(" Connected via Ethernet")
 
 # Proceed with main.py
 machine.main('main.py')
--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-from network import WLAN, ETH
 import binascii
 import time
 import machine
 import ujson
 from machine import RTC
 import pycom
-
-# Connect via ethernet
-eth = ETH()
-eth.init(hostname="itrich-pygate-001")
-eth.ifconfig(config="dhcp")
-
-# Set Gateway EUI if not already set
-if not 'gateway_eui' in pybytes_conf.keys():
-    wl = WLAN()
-    gateway_eui = binascii.hexlify(wl.mac().sta_mac)[:6] + 'fffe' + binascii.hexlify(wl.mac().sta_mac)[6:]
 
 print('\nStarting LoRaWAN concentrator')
 
@@ -24,15 +13,12 @@ def machine_cb (arg):
     if (evt & machine.PYGATE_START_EVT):
         # Green
         pycom.rgbled(0x103300)
-        pybytes.send_signal(0, 'Start')
     elif (evt & machine.PYGATE_ERROR_EVT):
         # Red
         pycom.rgbled(0x331000)
-        pybytes.send_signal(0, 'Error')
     elif (evt & machine.PYGATE_STOP_EVT):
         # RGB off
         pycom.rgbled(0x000000)
-        pybytes.send_signal(0, 'Stop')
 
 # register callback function
 machine.callback(trigger = (machine.PYGATE_START_EVT | machine.PYGATE_STOP_EVT | machine.PYGATE_ERROR_EVT), handler=machine_cb)
@@ -50,9 +36,6 @@ print(" OK", end='\n')
 # Read the GW config file from Filesystem
 with open('config.json','r') as fp:
     config = ujson.load(fp)
-
-config['gateway_conf'].update({'gateway_ID': gateway_eui})
-config['gateway_conf']['servers'][0].update({'gateway_ID': gateway_eui})
 
 # Start the Pygate
 machine.pygate_init(ujson.dumps(config))
