7c91a83fa024ed5150cc4b6399d8bacf6092dd4f
Eduard Itrich <eduard@itrich.net>
2022-11-23 20:36:51 +0100

    Connect via WiFi hotspot
    Signed-off-by: Eduard Itrich <eduard@itrich.net>

--- boot.py
+++ boot.py
@@ -1,16 +1,27 @@
 # boot.py -- run on boot-up
 import machine
-from network import ETH
+import time
+# from network import ETH
+from network import WLAN
 
-# Connect via ethernet
-eth = ETH()
-eth.init(hostname="itrich-pygate-001")
-eth.ifconfig(config="dhcp")
+# # Connect via ethernet
+# eth = ETH()
+# eth.init(hostname="itrich-pygate-001")
+# eth.ifconfig(config="dhcp")
+#
+# while not eth.isconnected():
+#     print('.', end='')
+#     time.sleep(1)
+# print(" Connected via Ethernet")
 
-while not eth.isconnected():
+# Connect via wifi
+wlan = WLAN(mode=WLAN.STA)
+wlan.connect(ssid='Ed', auth=(WLAN.WPA2, '20221123'))
+
+while not wlan.isconnected():
     print('.', end='')
     time.sleep(1)
-print(" Connected via Ethernet")
+print(" Connected via WiFi")
 
 # Proceed with main.py
 machine.main('main.py')
