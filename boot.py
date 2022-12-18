79e95f9b792af2c04004ff449669e8c010e99305
Eduard Itrich <eduard@itrich.net>
2022-12-18 19:34:06 +0100

    Switch back to ethernet as backhaul

--- boot.py
+++ boot.py
@@ -1,27 +1,27 @@
 # boot.py -- run on boot-up
 import machine
 import time
-# from network import ETH
-from network import WLAN
+from network import ETH
+# from network import WLAN
 
-# # Connect via ethernet
-# eth = ETH()
-# eth.init(hostname="itrich-pygate-001")
-# eth.ifconfig(config="dhcp")
-#
-# while not eth.isconnected():
-#     print('.', end='')
-#     time.sleep(1)
-# print(" Connected via Ethernet")
+# Connect via ethernet
+eth = ETH()
+eth.init(hostname="itrich-pygate-001")
+eth.ifconfig(config="dhcp")
 
-# Connect via wifi
-wlan = WLAN(mode=WLAN.STA)
-wlan.connect(ssid='Ed', auth=(WLAN.WPA2, '20221123'))
-
-while not wlan.isconnected():
+while not eth.isconnected():
     print('.', end='')
     time.sleep(1)
-print(" Connected via WiFi")
+print(" Connected via Ethernet")
+
+# # Connect via wifi
+# wlan = WLAN(mode=WLAN.STA)
+# wlan.connect(ssid='Ed', auth=(WLAN.WPA2, '20221123'))
+
+# while not wlan.isconnected():
+#     print('.', end='')
+#     time.sleep(1)
+# print(" Connected via WiFi")
 
 # Proceed with main.py
 machine.main('main.py')
